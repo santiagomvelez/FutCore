46c2d0c26706de07361d2a2534752572eba7061a
santiagomvelez <santiagomvelez1002@gmail.com>
2026-06-20 18:37:26 -0300

    creacion Arrays de equipo

---
+++ js/equipo.js
@@ -0,0 +1,19 @@
+//Archivo solo de los equipos
+
+const equipos = [
+    {id: 1, nombre: "River Plate", liga: "Liga Profesional de Futbol", pais: "Argentina", jugadores: [
+        {id: 41, nombre: "Santiago Beltrán", posicion: "Arquero", nacimiento: 4/10/2004, edad: 21, nacionalidad: "Argentina" },
+        {id: 33, nombre: "Ezequiel Centurión", posicion: "Arquero", nacimiento: 20/5/1997, edad: 29, nacionalidad: "Argentina-Italia"},
+        {id: 1, nombre: "Franco Armani", posicion: "Arquero", nacimeinto: 16/10/1986, edad: 39, nacionalidad: "Argentina"},
+        {id: 13, nombre: "Lautaro Rivero", posicion: "Defensa central", nacimiento: 1/11/2003, edad: 22, nacionalidad: "Argentina"},
+        {id: 28, nombre: "Lucas Martinez Quarta", posicion: "Defensa central", nacimiento: 10/5/1996, edad: 30, nacionalidad:"Argentina-Italia"},
+        {id: 2, nombre: "Tobias Ramirez", posicion: "Defensa central", nacimiento: 11/11/2006, edad: 19, nacionalidad: "Argentina"},
+
+    ] } ,
+
+    {id: 2, nombre: "Boca Juniors", liga: "Liga Profsional de Futbol", pais: "Argentina"},
+
+    {id: 3, nombre:"Velez Arfields", liga: "Liga Profesional de Futbol", pais: "Argentina"}
+
+
+]
